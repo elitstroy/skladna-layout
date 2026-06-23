1814902bc05fac024fd901d7ec085b61e7d2f198
Aleh Kliuiko <o.kliuiko@gmail.com>
2026-06-23 13:57:52 +0300

    feat: добавлены декларации для модулей SCSS и CSS
    - Создан файл `vite-env.d.ts` для определения типов модулей
    - Добавлены декларации для SCSS и CSS файлов, обеспечивающие корректную работу с ними в TypeScript
    - Повышена поддержка типов и улучшена разработка среды в проектах на Vite

---
+++ src/vite-env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="vite/client" />
+
+declare module '*.scss' {
+    const content: void;
+    export default content;
+}
+
+declare module '*.css' {
+    const content: void;
+    export default content;
+}
